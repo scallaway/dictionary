62a7ea50ca3ffa245bd21505dc4f6b03c1da7878
Scott Callaway <github@scottcallaway.co.uk>
2019-05-07 13:57:18 +0100

    API v2 upgrade and code refactor

--- util.js
+++ util.js
@@ -3,67 +3,37 @@ const config = require("./config.json");
 const https = require("https");
 
 const discord = {
-  getOnlineUsers: users => {
-    let onlineUsers = [];
-    users.forEach(user => {
-      if (user.presence.status === "online" && !user.bot) {
-        onlineUsers.push(user);
-      }
-    });
+  getOnlineUsers: users =>
+    users.filter(user => user.presence.status !== "offline" && !user.bot).size,
 
-    return onlineUsers.length;
-  },
-
-  getAllArgs: message => {
-    return message.content
+  getAllArgs: message =>
+    message.content
       .slice(config.prefix.length)
       .trim()
-      .split(/ +/g);
-  },
+      .split(/ +/g),
 
-  getCommand: args => {
-    return args.shift().toLowerCase();
-  },
+  getCommand: args => args.shift().toLowerCase(),
 
-  getVoiceChannels: channels => {
-    let voiceChannels = [];
+  getVoiceChannels: channels =>
+    channels.filter(channel => channel.type === "voice").size,
 
-    channels.forEach(channel => {
-      if (channel.type === "voice") {
-        voiceChannels.push(channel);
-      }
-    });
-
-    return voiceChannels.length;
-  },
-
-  getTextChannels: channels => {
-    let textChannels = [];
-
-    channels.forEach(channel => {
-      if (channel.type === "text") {
-        textChannels.push(channel);
-      }
-    });
-
-    return textChannels.length;
-  }
+  getTextChannels: channels =>
+    channels.filter(channel => channel.type === "text").size
 };
 
 const helpers = {
-  convertTime: time => {
-    return new Date(time).toString();
-  }
+  convertTime: time => new Date(time).toString()
 };
 
 const http = {
   getDefinition: (word, callback) => {
+    const { app_id, app_key } = config;
     const options = {
       hostname: "od-api.oxforddictionaries.com",
-      path: `/api/v1/entries/en/${word}/definitions`,
+      path: `/api/v2/entries/en-gb/${word}?fields=definitions`,
       headers: {
-        app_id: config.app_id,
-        app_key: config.app_key
+        app_id,
+        app_key
       }
     };
 
@@ -115,10 +85,9 @@ const http = {
     });
   },
 
-  generateDefinition: data => {
-    return JSON.parse(data).results[0].lexicalEntries[0].entries[0].senses[0]
-      .definitions[0];
-  }
+  generateDefinition: data =>
+    JSON.parse(data).results[0].lexicalEntries[0].entries[0].senses[0]
+      .definitions[0]
 };
 
 module.exports = {
